86644dfc689cbee29313deb6c80c6255f37060f2
Iruazu <ygnk0805@outlook.jp>
2025-04-01 22:33:36 +0900

    dev op add

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
-  base: "/REPOSITORY_NAME/",
+  base: "/my-portfolio/",
   site: "https://Iruazu.github.io/my-portfolio/",
 });
